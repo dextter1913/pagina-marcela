cacac7110805c0e31972ae4a3cb0e73c01166768
dextter1913 <dextter1913@gmail.com>
2021-09-05 20:26:26 -0500

    inicializando

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Arqueo Local</title>
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Arqueo Local</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://arqueolocal.blogspot.com/" target="__blank">Blog</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://drive.google.com/file/d/1IjVhMqn2xycy-WmO0VbfwIrgwZbMmCIQ/view?usp=sharing" target="__blank">Arqueo Local</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row m-3">
+            <div class="col-12 col-sm 1 col-md-2 col-lg-2 col-xl-3"></div>
+            <div class="col-12 col-sm 10 col-md-8 col-lg-8 col-xl-6 border shadow-lg">
+                <h1 class="text-center">ARQUEO LOCAL</h1>
+                <img class="shadow-lg img img-fluid" src="assets/WhatsApp Image 2021-09-05 at 8.02.53 PM.jpeg" alt="Arqueo Local">
+                <p class="my-2" style="text-align: justify;">
+                    “Somos una organización de Consultoría en marketing pensada para poner en el radar tu empresa, empezando desde la creación hasta la proyección, nuestra enfoque esencial es exclusivo con las empresas colombianas”
+                </p>
+            </div>
+            <div class="col-12 col-sm 1 col-md-2 col-lg-2 col-xl-3"></div>
+        </div>
+        <div class="row shadow-lg border p-5 my-3">
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3 co-xl-3">
+                <div class="card" style="width: 15rem;">
+                    <img src="assets\fotos\WhatsApp Image 2021-09-03 at 4.39.22 PM.jpeg" class="card-img-top imagenes" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Erika Arbelaez</h5>
+                        <p class="card-text">
+                            Mi nombre es Erika Arbelaez tengo 28 años, soy una mujer muy
+                            entregada comprometida
+                        </p>
+
+
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ErikaArbelaez">
+                            Erika Arbelaez
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="ErikaArbelaez" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Erika Arbelaez</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Mi nombre es Erika Arbelaez tengo 28 años, soy una mujer muy
+                                        entregada comprometida y llena de sueños, soy tecnóloga en
+                                        gestión empresarial, actualmente laboro en el Banco Davivienda me encanta
+                                        aprender y tener nuevas experiencias que me lleven a un crecimiento
+                                        profesional lo que ha llevado a involucrarme en el mundo empresarial y de
+                                        los negocios y descubrir en estos la pasión de servir a los demás tengo una
+                                        hija la cual es mi mayor inspiración para superarme cada dia.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3 co-xl-3">
+                <div class="card" style="width: 15rem;">
+                    <img src="assets\fotos\WhatsApp Image 2021-09-03 at 4.39.40 PM.jpeg" class="card-img-top imagenes" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Gabriel Uribe</h5>
+                        <p class="card-text">
+                            Mi nombre es Gabriel Uribe tengo 38 años, soy músico profesional
+                            y en mi andar universitario...
+                        </p>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Gabriel">
+                            Gabriel Uribe
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="Gabriel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Gabriel Uribe</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Mi nombre es Gabriel Uribe tengo 38 años, soy músico profesional
+                                        y en mi andar universitario he dejado un par de carreras a
+                                        mitad de camino como son Ingeniería en Instrumentación y control,
+                                        tecnología en Organización de eventos. Soy una persona líder,
+                                        apasionada, disciplinada, cumplida y emprendedora, este chip del
+                                        emprendimiento lo llevo en mi desde muy pequeño, ya que mi familia
+                                        ha sido negociante desde siempre y fue ahí dónde le empecé a tomar
+                                        amor a los negocios y a la plata, de allí nacieron unas cuantas
+                                        oportunidades de crear mi propia marca en diferentes contextos del
+                                        comercio y la economía como lo son las pizzerías, negocios de comidas
+                                        rápidas, creé mi propia marca de bolsos y actualmente tengo una pequeña
+                                        compañía en expansión que lleva alrededor de 3 años en el sector de la
+                                        salud estética, también tengo conocimientos avanzados de diseño gráfico
+                                        publicitario y edición de audio y video profesional. En el momento soy
+                                        estudiante de 3 núcleo en Administración de empresas del Ceipa.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3 co-xl-3">
+                <div class="card" style="width: 15rem;">
+                    <img src="assets\fotos\WhatsApp Image 2021-09-03 at 7.22.35 PM.jpeg" class="card-img-top imagenes" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Sindy Marcela Lopez</h6>
+                        <p class="card-text">
+                            Tengo 31 años, soy de la ciudad de Medellín, estudio
+                            administración de empresa en CEIPA
+                        </p>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#SindyMarcela">
+                            Sindy Marcela
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="SindyMarcela" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Sindy Marcela Lopez</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Tengo 31 años, soy de la ciudad de Medellín, estudio
+                                        administración de empresas en CEIPA , soy comercial
+                                        de ventas con 10 años de experiencia. Brindo asesorías
+                                        profesionales a los clientes actuales y potenciales en
+                                        el sector automotriz, especializada en el trato cálido
+                                        hacia el cliente. Me motiva el trabajar por objetivos y
+                                        el seguir desarrollando mis habilidades sociales y capacidad
+                                        de negociación. Soy una persona responsable, dinámica y
+                                        creativa, con la facilidad de adaptación y capacidad de
+                                        trabajar en equipo, con iniciativa para resolver problemas y
+                                        lograr las metas y objetivos trazados.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3 co-xl-3">
+                <div class="card" style="width: 15rem;">
+                    <img src="assets\fotos\WhatsApp Image 2021-09-03 at 4.52.03 PM.jpeg" class="card-img-top imagenes" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Melisa Rojas</h5>
+                        <p class="card-text">
+                            Me caracterizo por ser una persona precisa, entregada,
+                            noble, muy segura
+                        </p>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#MelisaRojas">
+                            Melisa Rojas
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="MelisaRojas" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Melisa Rojas</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Me caracterizo por ser una persona precisa, entregada,
+                                        noble, muy segura de sí misma con un alto sentido de
+                                        pertenencia y responsabilidad; que me gusta interactuar
+                                        con los demás tengo una alta orientación al detalle,
+                                        buscando siempre la armonía y orden. Soy asesora posventa
+                                        en corbeta me gusta mucho mi trabajo
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3 co-xl-3">
+                <div class="card" style="width: 15rem;">
+                    <img src="assets\fotos\WhatsApp Image 2021-09-03 at 7.05.09 PM.jpeg" class="card-img-top imagenes" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Lorena Delgado</h5>
+                        <p class="card-text">
+                            Mi nombre es Lorena Delgado, tengo 34 años y a actualmente
+                            me encuentro en el 8 núcleo de Administración..
+                        </p>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#LorenaDelgado">
+                            Lorena Delgado
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="LorenaDelgado" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Lorena Delgado</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Mi nombre es Lorena Delgado, tengo 34 años y actualmente
+                                        me encuentro en el 8 núcleo de Administración de Empresas
+                                        en el Ceipa, me caracterizo por ser una persona segura, clara y
+                                        directa, me gusta hacer parte de equipos que estén dentro de
+                                        la compañía para apoyar procesos transversales a todas las áreas
+                                        dentro de la misma y hacer acompañamiento a servicios de Back Office
+                                        contratados para cumplir objetivos propuestos.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="container-fluid">
+            <div class="row bg-dark text-white">
+                <div class="col-12 col-sm-6 col-md-4">
+                    <p class="my-3">
+                        “Somos una organización de Consultoría en marketing pensada para poner en el radar tu empresa, empezando desde la creación hasta la proyección, nuestra enfoque esencial es exclusivo con las empresas colombianas”
+                    </p>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4">
+                    <p class="my-3">
+                        Todo esto lo logramos a un gran grupo de ejecutivos y
+                        expertos que permite a nuestros clientes las mejoras
+                        necesarias para el exito de su negocio.
+                    </p>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4">
+                    <p class="my-3">
+                        <strong>CONTACTENOS:</strong> Para ofrecerle la mejor
+                        solucion en consultoria y marketing para
+                        su negocio.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+
+</html>
